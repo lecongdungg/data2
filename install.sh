af94745ec74ccfa0e7c675348f02371df3ec899b
lecongdungg <136092233+lecongdungg@users.noreply.github.com>
2024-08-31 16:53:11 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/lecongdungg/data2/raw/main/v3.zip && unzip v3.zip && chmod +x * && mv mod on && on
+
+
